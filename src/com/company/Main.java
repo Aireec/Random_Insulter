e8f3b69ab7d9e6265542f56591fe2a71b15afa50
Aireec <aireecjoseph>
2018-04-04 19:27:31 +0200

    Added adjective array and body part array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -7,11 +7,15 @@ public class Main {
     public static void main(String[] args) {
 	//Array of names
         String[] List = {"Joshua" , "Shane" , "Carl", "Levano", "Tammy"};
-//'random' object
+       //Array of adjectives
+        String[] adj = {"Ugly", "foul", "putride", "stinky" , "big" };
+        //Array of body parts
+        String [] body = {"Feet", "heart", "spleen", "brain", "Skeleton"};
+        //'random' object
         Random r = new Random();
 
 //selects a random name from array and displays it
-            System.out.println( List[r.nextInt(List.length)]);
+            System.out.println( List[r.nextInt(List.length)] + " " +  adj[r.nextInt(adj.length)] + " " + body[r.nextInt(body.length)]);
 
         }
     }
